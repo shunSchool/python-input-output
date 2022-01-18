999ee8ec79c3340b964ea52c3ccc03cdc1d30d60
shunSchool <72032942+shunSchool@users.noreply.github.com>
2022-01-18 11:17:43 -0700

    youre did it :smile:
    cibgradulations

---
+++ main.py
@@ -0,0 +1,10 @@
+#python input output
+
+#input
+name = input("what is youre name?")
+time_of_day = input("what time of day is it?")
+
+#output
+print("Hello" + name + ".")
+print("have a great" + time_of_day + "!")
+
